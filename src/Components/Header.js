dc740db6785b09a1430a4eaf691e8cb29a171ee6
Anmol <anmol.singh@thewitslab.com>
2022-09-13 15:44:58 +0530

    somechangesonanimation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,50 @@
 import React from "react";
 import "./HeaderStyle.css";
 
+import { motion } from "framer-motion";
+
 const Header = () => {
   return (
     <header>
-      <nav>
+      <motion.nav>
         <div class="logo">
-          <h1 class="animated_infinite_bounce">Wits Innovation Lab</h1>
+          <motion.h1
+            animate={{ scale: [1.1, 0.8, 1.1] }}
+            whileHover={{ scale: [1, 1, 1] }}
+            transition={{ duration: 1.2, repeat: Infinity }}
+          >
+            Wits Innovation Lab
+          </motion.h1>
         </div>
         <div class="menu">
-          <a href="/">About Us</a>
-          <a href="/">Industry</a>
-          <a href="/">Services</a>
-          <a href="/">Career</a>
+          <motion.a whileHover={{ scale: 1.1 }} href="/">
+            About Us
+          </motion.a>
+          <motion.a whileHover={{ scale: 1.1 }} href="/">
+            Industry
+          </motion.a>
+          <motion.a whileHover={{ scale: 1.1 }} href="/">
+            Services
+          </motion.a>
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 100 }}
+            href="/"
+          >
+            Career
+          </motion.a>
         </div>
-      </nav>
+      </motion.nav>
 
       <main>
         <section>
-          <h3>We're not just IT.</h3>
+          <motion.h3
+            initial={{ scale: 0.2, opacity: 0.1 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            We're not just IT.
+          </motion.h3>
           <h1>
             We're a Business Transformation Company.
             <span class="change_content"> </span>
@@ -29,12 +55,28 @@ const Header = () => {
             individuals who share our love for connecting people with the best
             in their area.
           </p>
-          <a href="/" class="btnone">
-            Let’s Connect
-          </a>
-          <a href="/" class="btntwo">
-            Tell Us About your Project
-          </a>
+          <motion.div className="btns">
+            <motion.a
+              initial={{ x: -550, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.2 }}
+              href="/"
+              class="btnone"
+            >
+              Let’s Connect
+            </motion.a>
+            <motion.a
+              initial={{ x: 550, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.2 }}
+              href="/"
+              class="btntwo"
+            >
+              Tell Us About your Project
+            </motion.a>
+          </motion.div>
         </section>
       </main>
     </header>
